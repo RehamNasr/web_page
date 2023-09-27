0246a3c3d5722a69236a0760f0fb0f85380ccde9
Reham Nasr <107366478+RehamNasr@users.noreply.github.com>
2023-09-27 19:45:23 +0300

    Add files via upload

---
+++ js/sty.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items:1,
+      autoplay:true,
+      loop:true,
+      margin:5,
+      // nav:true,
+      dots:true,
+      autoplayTimeout:1000
+      
+    });
+  });
